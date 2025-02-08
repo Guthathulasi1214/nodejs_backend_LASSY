2888c5340e3e58d17f029c0cf537552bdf021a7a
Guthathulasi1214 <thulasichowdary14@gmail.com>
2025-02-08 16:55:33 +0530

    PORT CHANGED

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const mongoose=require('mongoose');
 const vendorRoutes=require('./routes/vendorRoutes.js');
 const bodyParser=require('body-parser');
 const firmRoutes=require('./routes/firmRoutes.js');
-const productRoutes=require('./routes/productRoutes.js')
+const productRoutes=require('./routes/productRoutes.js');
+const cors=require('cors');
+const path=require('path')
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 dotEnv.config()
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("mongodb connected succesfully"))
@@ -15,10 +17,10 @@ app.use(bodyParser.json());
 app.use('/vendor',vendorRoutes);
 app.use('/firm',firmRoutes);
 app.use('/product',productRoutes)
-
+app.use('/uploads',express.static('uploads'))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-app.use('/home',(req,res)=>{
+app.use('/',(req,res)=>{
     res.send("<h1>im thulsii</h1>")
 })
